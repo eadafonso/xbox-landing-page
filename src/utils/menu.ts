e832480337a761bc5c27acf8a33852197f0a8bd7
Edvaldo Afonso <eadafonso@gmail.com>
2023-12-05 21:15:51 +0100

    feat: create link_menu array

---
+++ src/utils/menu.ts
@@ -0,0 +1,21 @@
+export const link_menu = [
+  {
+    name: "Browse",
+    link: "#",
+  },
+
+  {
+    name: "Games",
+    link: "#",
+  },
+
+  {
+    name: "ContRoller",
+    link: "#",
+  },
+
+  {
+    name: "Inside",
+    link: "#",
+  },
+];
